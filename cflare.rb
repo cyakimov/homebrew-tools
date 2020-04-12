615817e26d8ca7bfeb4efeaaea149d1bf0e864cd
Carlos Yakimov <ceyakimov@falabella.cl>
2020-04-11 20:29:06 -0400

    Bump cflare to 0.2.0

--- cflare.rb
+++ cflare.rb
@@ -1,15 +1,15 @@
 class Cflare < Formula
   desc "Cloudflare command-line tool"
   homepage "https://github.com/cyakimov/cloudflare-cli"
-  version "0.1.0"
+  version "0.2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cyakimov/cloudflare-cli/releases/download/v0.1.0/cflare-v0.1.0-x86_64-apple-darwin.tar.gz"
-    sha256 "3c09993f7c747348d9ce6b2dfb4892e8acbaf9decf597538ab45bf38819a0d76"
+    url "https://github.com/cyakimov/cloudflare-cli/releases/download/v0.2.0/cflare-v0.2.0-x86_64-apple-darwin.tar.gz"
+    sha256 "aed3984f9655f8ce51746a134d5dcb8d3fece5e9c3eeac6a61fd152466e8736d"
   elsif OS.linux?
-	url "https://github.com/cyakimov/cloudflare-cli/releases/download/v0.1.0/cflare-v0.1.0-x86_64-unknown-linux-musl.tar.gz"
-	sha256 "03666b2932392c7d25f21a799b216b57bf20d20edbaf6abd2b3c5afc8ec056c6"
+	url "https://github.com/cyakimov/cloudflare-cli/releases/download/v0.2.0/cflare-v0.2.0-x86_64-unknown-linux-musl.tar.gz"
+	sha256 "488f09384dcc9462a4aa3112d8d6917bb6bbc9ac9d984c91dfeab07270dcc82d"
   end
 
   def install
